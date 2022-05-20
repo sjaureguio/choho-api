ee8edc8bd86d5b594f2bdf50136123974379fde3
Solano Jauregui <szjaureguiosores@gmail.com>
2022-05-20 17:58:30 -0500

    Agrega Controller y endPoint de tipos de producto

---
+++ app/Http/Controllers/ProductTypeController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Advisor;
+
+class ProductTypeController extends Controller
+{
+    /**
+     * Obtenemos todos los asesores de la BD
+     */
+    public function records()
+    {
+        $records = app('db')->select("SELECT id, name FROM advisors");
+
+        return response()->json([
+            'success' => true,
+            'message' => 'OK',
+            'data' => $records
+        ], 200);
+    }
+
+    /**
+     * Método para obtener el asesor y actualizarlo
+     * @params $id
+     */
+    public function record($id)
+    {
+        return Advisor::findOrFail($id);
+    }
+
+    /**
+     * Método para crear o actulizar asesor
+     */
+    public function store(Request $request)
+    {
+        $id = $request->input('id');
+        $product_type = Advisor::firstOrNew(['id' => $id]);
+        $product_type->fill($request->all());
+        $product_type->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => ($id)
+                ? 'Asesor editado con éxito'
+                : 'Asesor registrado con éxito',
+            'id' => $product_type->id
+        ], 200);
+    }
+
+    /**
+     * Método para eliminar asesor
+     * @params $id
+     */
+    public function destroy($id)
+    {
+        $product_type = Advisor::findOrFail($id);
+        $product_type->update([
+            'status' => 0
+        ]);
+
+        return [
+            'success' => true,
+            'message' => 'Asesor eliminado con éxito'
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    // return $router->app->version();
+    return response()->json([
+        'success' => true,
+        'message' => 'Not found'
+    ], 200);
 });
+
+// CRUD tabla product_types (Tipos de producto)
+$router->get('/api/product-types', 'ProductTypeController@records');
+$router->get('/api/product-types/{id}', 'ProductTypeController@record');
+$router->post('/api/product-types', 'ProductTypeController@store');
+$router->delete('/api/product-types/{id}', 'ProductTypeController@destroy');
+
